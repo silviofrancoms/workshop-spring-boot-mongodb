a797c886c993bde57205a529d08b75243f33c514
silviofrancoms <silviofrancoms@gmail.com>
2021-12-22 18:55:10 -0400

    Obtendo um usuário por id

---
+++ src/main/java/com/silviofranco/workshopmongo/resources/exceptions/StandardError.java
@@ -0,0 +1,25 @@
+package com.silviofranco.workshopmongo.resources.exceptions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class StandardError {
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
