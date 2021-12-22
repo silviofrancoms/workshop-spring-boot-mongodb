02ee5d0532c799887aeee1908b7b924b57d2ad7b
silviofrancoms <silviofrancoms@gmail.com>
2021-12-22 13:59:35 -0400

    Conectando ao MongoDB com repository e service

--- src/main/java/com/silviofranco/workshopmongo/domain/User.java
+++ src/main/java/com/silviofranco/workshopmongo/domain/User.java
@@ -3,15 +3,17 @@ package com.silviofranco.workshopmongo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 
-
+@Document(collation ="user")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class User implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/silviofranco/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.silviofranco.workshopmongo.repository;
+
+import com.silviofranco.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
