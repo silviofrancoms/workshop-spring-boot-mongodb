a527de41b381a05547ce1de2bdf8ca1129cf852c
silviofrancoms <silviofrancoms@gmail.com>
2021-12-29 07:41:31 -0400

    Referenciando os posts do usuário!

--- src/main/java/com/silviofranco/workshopmongo/domain/User.java
+++ src/main/java/com/silviofranco/workshopmongo/domain/User.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Document
 public class User implements Serializable {
@@ -19,5 +21,12 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
